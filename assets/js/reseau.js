972bc81162798a1115f106d0b23bdd0d7e74971a
CyrusWeir <cyrusweir@gmail.com>
2024-10-16 11:44:11 +0200

    CSS optimisations and functionalities - esp hover and links, along with 'hide' functionality. added library items - blue and green like/dislike icons

--- assets/js/reseau.js
+++ assets/js/reseau.js
@@ -1,13 +1,24 @@
+// image variables
 const previewImages = document.getElementById("images-of-projects");
 const slide1 = 'url("/assets/images/websiteScreenshot.png")';
 const slide2 = 'url("/assets/images/screenshotNatureSite.png")';
 const slide3 = 'url("/assets/images/screenshotKoala.png")';
-console.log(previewImages);
 
+// form variables
+const retrieveUser = localStorage.getItem("user");
+const parseUser = JSON.parse(retrieveUser);
+console.log(parseUser);
+
+// button variables
 const likeButton = document.getElementById("like");
-
 const dislikeButton = document.getElementById("dislike");
+const hideButton = document.getElementById("hide-button")
 
+//text elements to be changed
+const profileText = document.querySelector("#about-me")
+
+
+//image carousel
 previewImages.style.backgroundImage = slide1;
 
 previewImages.addEventListener("click", function () {
@@ -24,3 +35,29 @@ previewImages.addEventListener("click", function () {
     previewImages.backgroundImage = slide1;
   }
 });
+
+
+//like/dislike button behaviours
+likeButton.addEventListener("click", function () {
+  // if (likeButton.src == "/assets/images/emoji-kiss-fill-pink.svg") {
+  console.log("works");
+  likeButton.src = "/assets/images/likeGreenSvg.svg"
+  // } else { likeButton.src = "/assets/images/emoji-kiss-fill-pink.svg" }
+})
+
+dislikeButton.addEventListener("click", function () {
+  console.log("works also")
+  dislikeButton.src = "/assets/images/dislikeBlueSvg.svg"
+})
+
+//hide button
+hideButton.addEventListener("click", function () {
+  console.log("hide button activated")
+  if (profileText.style.display === "block") {
+    profileText.style.display = "none";
+    hideButton.textContent = "agrandir"
+  } else {
+    profileText.style.display = "block";
+    hideButton.textContent = "reduire"
+  }
+})
